3f283eaa71d2b66096e2f25a78117c675b3da218
StrAVengeR <batbayrbilguun8@gmail.com>
2025-05-01 04:57:47 +0800

    Update ZooApp.java

--- src/ZooApp.java
+++ src/ZooApp.java
@@ -1,15 +1,24 @@
+import java.util.ArrayList;
 import java.util.List;
 
-class Animal {
-    public String getName() {
-        return this.getClass().getSimpleName();
+class Lion extends Animal {}
+class Crane extends Animal {}
+
+public class ZooApp {
+    public static void main(String[] args) {
+        List<Animal> zoo1 = new ArrayList<>();
+        zoo1.add(new Lion());
+        zoo1.add(new Crane());
+
+        List<Animal> zoo2 = new ArrayList<>();
+
+        Zoo<Animal> zoo = new Zoo<>();
+        zoo.transferAnimals(zoo1, zoo2);
+
+        for (Animal a : zoo2) {
+            System.out.println(a.getName());
+        }
     }
 }
 
-public class Zoo<T extends Animal> {
-    public void transferAnimals(List<? extends T> source, List<? super T> destination) {
-        destination.addAll(source);
-        source.clear();
-    }
-}
 
