7922646bb8fe19e18611eb101f51f61b677c19cd
StrAVengeR <batbayrbilguun8@gmail.com>
2025-05-01 05:09:52 +0800

    Update LibApp.java

--- src/LibApp.java
+++ src/LibApp.java
@@ -1,11 +1,11 @@
 public class LibApp {
-	public static void main(String[] args) {
-		Library lib = new Library();
-		lib.addBook("Гэдэс", 1921);
-		lib.addBook("1984", 1949);
-		lib.addBook("Сүүн Зам", 2020);
-		for (Book b : lib.getRecentBooks(2000)) {
-			System.out.println(b.getTitle() + ": " + b.getYear());
-		}
-	}
+    public static void main(String[] args) {
+        Library lib = new Library();
+        lib.addBook(new Book("Suun zam", 2020));
+        lib.addBook(new Book("Mongoliin nuuts towcoo", 1980));
+
+        for (Book b : lib.getBooksAfter(2000)) {
+            System.out.println(b);
+        }
+    }
 }
